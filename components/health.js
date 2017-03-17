566ec841574ce6d2fdc2e9d241991890267037c2
Joe Hand <joe@joeahand.com>
2017-03-17 10:50:37 -0700

    use localcast to show health for single archive

---
+++ components/health.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const choo = require('choo')
+const html = require('choo/html')
+const css = require('sheetify')
+const prettyBytes = require('pretty-bytes')
+const circle = require('../elements/circle')
+
+css('tachyons')
+css`
+  body {
+    color: #293648;
+    font-family: 'Source Sans Pro', 'Helvetica Neue', 'Lucida Grande', Arial, sans-serif;
+  }
+  .pulse-circle {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    box-shadow: 0 0 0 rgba(205, 236, 255, 0.6);
+    animation: pulse 2s 1s infinite;
+  }
+  .pulse-circle:hover {
+    animation: none;
+  }
+
+  @-webkit-keyframes pulse {
+    0% {
+      -webkit-box-shadow: 0 0 0 0 rgba(205, 236, 255, 1);
+    }
+    70% {
+        -webkit-box-shadow: 0 0 0 10px rgba(205, 236, 255, 0);
+    }
+    100% {
+        -webkit-box-shadow: 0 0 0 0 rgba(205, 236, 255, 0);
+    }
+  }
+  @keyframes pulse {
+    0% {
+      -moz-box-shadow: 0 0 0 0 rgba(205, 236, 255, 1);
+      box-shadow: 0 0 0 0 rgba(205, 236, 255, 1);
+    }
+    70% {
+        -moz-box-shadow: 0 0 0 20px rgba(205, 236, 255, 0);
+        box-shadow: 0 0 0 20px rgba(205, 236, 255, 0);
+    }
+    100% {
+        -moz-box-shadow: 0 0 0 0 rgba(205, 236, 255, 0);
+        box-shadow: 0 0 0 0 rgba(205, 236, 255, 0);
+    }
+  }
+`
+
+module.exports = healthView
+
+const grids = []
+
+function healthView (state, emit) {
+  if (!state.connected || !state.data) return ''
+
+  return html`
+    <div>
+      <section class="w-75 center tc pt4 pb3">
+        <p class="ttu tracked code">${statusMsg(state)}</p>
+      </section>
+      <section>
+        ${healthDisplay(state)}
+      </section>
+    </div>
+  `
+}
+
+function statusMsg (state) {
+  return state.connected
+          ? state.data && state.data.blocks
+            ? 'dat://' + state.key
+            : 'LOADING... (please wait few seconds while we connect)'
+          : "something went terribly wrong (with joe's logic). =("
+}
+
+function healthDisplay (state) {
+  if (!state.data) return ''
+  const health = state.data
+  return html`
+  <div>
+    <article class="cf ph1">
+      <div class="fl w-100 w-40-ns pl5">
+        <dl class="db w-auto-l lh-title">
+          <dd class="f6 fw4 ml0">Connected Peers</dd>
+          <dd class="f2 f-subheadline-l fw6 ml0">${health.peers.length}</dd>
+        </dl>
+        <dl class="db w-auto-l lh-title">
+          <dd class="f6 fw4 ml0">Total Size</dd>
+          <dd class="f2 f-subheadline-l fw6 ml0">${prettyBytes(health.bytes)}</dd>
+        </dl>
+        <dl class="db w-auto-l lh-title">
+          <dd class="f6 fw4 ml0">Blocks</dd>
+          <dd class="f2 f-subheadline-l fw6 ml0">${health.blocks}</dd>
+        </dl>
+        ${state.connected ? html`<p class="fw4 gray"><span class="v-mid mr2 bg-lightest-blue dib pulse-circle"></span>autoupdating</p>` : ''}
+      </div>
+      <div class="fl w-100 w-60-ns tc">
+        ${health.peers.map((peer, i) => {
+          const prog = peer.have * 100/peer.blocks
+          return circle(prog)
+        })}
+      </div>
+    </article>
+  </div>
+  `
+}
--- index.js
+++ index.js
@@ -1,59 +1,82 @@
-const bankai = require('bankai')
-const merry = require('merry')
-const path = require('path')
-const Health = require('hyperhealth')
-const lru = require('lru')
+const choo = require('choo')
+const html = require('choo/html')
+const css = require('sheetify')
+const localcast = require('localcast')
+const health = require('./components/health')
 
-const notFound = merry.notFound
-const error = merry.error
-const env = merry.env({ PORT: 8080 })
-const cache = lru(100)
+css('tachyons')
 
-const entry = path.join(__dirname, 'client.js')
-const assets = bankai(entry)
-const app = merry()
+const cast = localcast('hyperhealth')
+const app = choo()
+app.use(log)
+app.use(onConnection)
+app.use(dataStore)
 
-// TODO: pool swarms
-cache.on('evict', function (item) {
-  item.swarm.close()
-  item.archive.close()
-})
+app.route('/', mainView)
+app.mount('body')
 
-app.router([
-  ['/404', notFound()],
-  ['/', function (req, res, ctx, done) {
-    done(null, assets.html(req, res))
-  }],
-  ['/:key', function (req, res, ctx, done) {
-    done(null, assets.html(req, res))
-  }],
-  ['/bundle.js', (req, res, ctx, done) => {
-    done(null, assets.js(req, res))
-  }],
-  ['/bundle.css', (req, res, ctx, done) => {
-    done(null, assets.css(req, res))
-  }],
-  ['/api/:key', getHealth]
-])
+function mainView (state, emit) {
+  const welcome = html`
+    <article class="pa3 ph5-ns mt4">
+      <h3 class="f6 ttu tracked mt0">Getting Started</h3>
+      <p class="measure f4 f3-ns lh-copy">
+        Install hyperhealth via npm: <span class="code">npm install -g hyperhealth</span>.
+      </p>
+      <p class="measure f5 f4-ns lh-copy">
+        Run <span class="code">hyperhealth [dat-key]</span> to view the health for a Dat archive.
+        This page should automagically update and show the health!
+      </p>
+    </article>
+  `
 
-app.listen(env.PORT)
+  return html`
+    <body>
+      <main>
+        <header class="w-100 pa3 ph5-ns bg-white bb b--black-10">
+          <div class="db dt-ns mw9 center w-100">
+            <div class="db dtc-ns v-mid tl w-50">
+              <a href="/" class="dib f5 f4-ns fw6 mt0 mb1 link black-70" title="Home">
+                hyperhealth
+                <div class="dib">
+                  <small class="nowrap f6 mt2 mt3-ns pr2 black-70 fw2">web</small>
+                </div>
+              </a>
+            </div>
+            <nav class="db dtc-ns v-mid w-100 tl tr-ns mt2 mt0-ns">
+              <a title="About" href="https://github.com/karissa/hyperhealth" class="f6 fw6 hover-blue link black-70 mr2 mr3-m mr4-l dib">
+                About
+              </a>
+            </nav>
+          </div>
+        </header>
+        ${state.connected ? health(state, emit) : welcome}
+      </main>
+    </body>
+  `
+}
 
-function getHealth (req, res, ctx, done) {
-  app.log.info(ctx)
-  if (!ctx.params.key) return done(error({statusCode: 400, message: 'key required'}))
+function log (state, emitter) {
+  emitter.on('*', function (messageName, data) {
+    console.log('event', messageName, data)
+  })
+}
 
-  var health = cache.get(ctx.params.key)
-  if (!health) {
-    health = Health(ctx.params.key)
-    cache.set(ctx.params.key, health)
-  }
+function onConnection (state, emitter) {
+  cast.on('localcast', peer => {
+    state.connected = true
+    console.info(peer)
+  })
 
-  const results = health.get()
-  if (results) return done(null, results)
+  cast.on('key', key => {
+    state.key = key
+  })
+}
 
-  req.setTimeout(2000, () => {
-    // May need to wait for content to populate on new healths
-    if (results) return done(null, results)
-    done(error({statusCode: 500, message: 'Could not get health?'}))
+function dataStore (state, emitter) {
+  emitter.on('DOMContentLoaded', function () {
+    cast.on('data', data => {
+      state.data = data
+      emitter.emit('render')
+    })
   })
 }
